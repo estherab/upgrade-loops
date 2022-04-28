7cd4e381a0d67c46900f4084aec794fa3190025f
Esther <esther.abenito@gmail.com>
2022-04-28 16:19:36 +0200

    iteration 2

--- loops.js
+++ loops.js
@@ -5,3 +5,22 @@ for (let item of products) {
     if (item.includes("Camiseta"))
         console.log(item);
 }
+
+// Iteración 2
+const alumns = [
+    {name: 'Pepe Viruela', T1: false, T2: false, T3: true}, 
+		{name: 'Lucia Aranda', T1: true, T2: false, T3: true},
+		{name: 'Juan Miranda', T1: false, T2: true, T3: true},
+		{name: 'Alfredo Blanco', T1: false, T2: false, T3: false},
+		{name: 'Raquel Benito', T1: true, T2: true, T3: true}
+]
+
+for (let key in alumns) { 
+    if ((alumns[key].T1 && alumns[key].T2) || (alumns[key].T1 && alumns[key].T3) || (alumns[key].T2 && alumns[key].T3))
+        alumns[key].isApproved = true;
+
+    else 
+        alumns[key].isApproved = false;
+}
+
+console.log(alumns);
