f5271a705b668608651b9088bdf69f36d1157e4f
Esther <esther.abenito@gmail.com>
2022-04-28 16:54:58 +0200

    iteration 7

--- loops.js
+++ loops.js
@@ -77,5 +77,22 @@ for (let toy of toys) {
 
 console.log(toys);
 
+// Iteración 7
+const popularToys = [];
+const toys2 = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+
+for (let toy of toys2) {
+    if (toy.sellCount > 15)
+        popularToys.push(toy);
+}
+
+console.log(popularToys);
+
 
 
