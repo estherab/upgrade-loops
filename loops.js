25f93a26db8cc1ca92ba6afce2f54a608c076426
Esther <esther.abenito@gmail.com>
2022-04-28 16:10:07 +0200

    iteration 1

---
+++ loops.js
@@ -0,0 +1,7 @@
+// Iteración 1
+const products = ['Camiseta de Pokemon', 'Pantalón coquinero', 'Gorra de gansta', 'Camiseta de Basket', 'Cinrurón de Orión', 'AC/DC Camiseta']
+
+for (let item of products) {
+    if (item.includes("Camiseta"))
+        console.log(item);
+}
