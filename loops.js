be0eba156f90c7a363693c51e94f239028dfb8a3
Esther <esther.abenito@gmail.com>
2022-04-28 16:52:00 +0200

    iteration 6

--- loops.js
+++ loops.js
@@ -61,4 +61,21 @@ for (let i = 0; i < placesToTravel2.length; i++) {
 
 console.log(placesToTravel2);
 
+// Iteración 6
+const toys = [
+    {id: 5, name: 'Buzz MyYear'}, 
+    {id: 11, name: 'Action Woman'}, 
+    {id: 23, name: 'Barbie Man'}, 
+    {id: 40, name: 'El gato con Guantes'},
+    {id: 40, name: 'El gato felix'}
+    ]
+
+for (let toy of toys) { 
+    if (toy.name.includes("gato"))
+        toys.splice(toys.indexOf(toy), 1);
+}
+
+console.log(toys);
+
+
 
